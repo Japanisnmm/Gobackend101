1283583c6806adebdba205f9287178f596b5ce06
“Japanisnmm <karn12112013@gmail.com>
2024-01-08 18:18:26 +0700

    second commit

--- main.go
+++ main.go
@@ -1,7 +1,30 @@
 package main
 
+import (
+	
+	"os"
 
+	"github.com/Japanisnmm/GoBackend101/config"
+	"github.com/Japanisnmm/GoBackend101/modules/servers"
+	"github.com/Japanisnmm/GoBackend101/pkg/databases"
+)
+
+
+func envPath() string {
+	if len(os.Args) == 1 {
+		return ".env"
+	} else {
+		return os.Args[1]
+	}
+}
 
 func main() {
+	cfg := config.LoadConfig(envPath())
+
+	db := databases.DbConnect(cfg.Db())
+	defer db.Close()
+
+	servers.NewServer(cfg,db).Start()
+	
 
 }
--- pkg/databases/db.go
+++ pkg/databases/db.go
@@ -1,1 +1,23 @@
 package databases
+
+import (
+	"log"
+	"github.com/Japanisnmm/GoBackend101/config"
+	
+	
+	_ "github.com/jackc/pgx/v5/stdlib"
+	"github.com/jmoiron/sqlx"
+)
+
+
+func DbConnect(cfg config.IDbConfig) *sqlx.DB {
+//connect
+   db, err := sqlx.Connect("pgx", cfg.Url())
+   if err != nil {
+	log.Fatalf("connect to db failed : %v",err)
+}
+db.DB.SetMaxOpenConns(cfg.MaxOpenConns())
+return db
+}
+
+
